fb0f104d08c7ecef0ea5ca3096649c324c7a2a06
nadianingtias <nadia.ningtias20@gmail.com>
2019-11-12 12:25:44 +0700

    nomor 2 selesai

--- JCDS_01_exam_2.py
+++ JCDS_01_exam_2.py
@@ -4,28 +4,32 @@ def sep():
 def cekJumlahKarakter(input):
     lenInput = len(input)
     bisa = False
+    counter =0
     for i in range(1, lenInput):
         # print(lenInput)
         lenInput -=i
+        counter +=1
         if lenInput == 0:
             bisa = True
         if lenInput < 0 :
             break
     # print(bisa)
-    return bisa
+    return bisa, counter
 
 def segitigaKata(inputKal):
     input = inputKal.replace(' ', '')
     lenInput = len(input)
     # print(cekJumlahKarakter(inputKal) )
-    if not cekJumlahKarakter(input) :
+    cekJumlah, baris = cekJumlahKarakter(input)
+    print("ini ",baris)
+    if not cekJumlah :
         print("===== Mohon maaf, jumlah karakter tidak memenuhi syarat membentuk pola. =====")
     else:
         # print("lanjut")
         start = 0
         stop=0
         counter = 0
-        for i in range(1, int(lenInput/2)):
+        for i in range(1, baris):
             counter+=1
             stop += i
             # print(input[start:stop]+ " ",)
@@ -36,24 +40,25 @@ def segitigaKata(inputKal):
             start = stop
         
         cc = counter
-        print(stop)
+        # print(stop)
         stop = stop-1
         start =0
-        print(start+counter)
+        # print(start+counter)
         for i in range(0, cc+1):
             # stop = counter
-            print(input[start:start+counter])
-            # x = list(input[start:start+counter])
-            # for h in x :
-            #     print(h+ ' ', end='')
-            # print('')
+            # print(input[start:start+counter])
+            x = list(input[start:start+counter])
+            for h in x :
+                print(h+ ' ', end='')
+            print('')
             start = counter + start
             counter -=1
 
         
 
 def main():
-    kalimat = input("masukkan kalimat : ")
+    # kalimat = input("masukkan kalimat : ")
+    kalimat = "Purwadhika Startup and Coding School @BSD"
     sep()
     print("kalimat : ", kalimat)
     segitigaKata(kalimat)
